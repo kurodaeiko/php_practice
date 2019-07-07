98eeb6f9fdb9e91b1d153fdbfe3a08456964a390
kuroda <watashihawatashijp@yahoo.co.jp>
2019-07-07 11:53:34 +0000

    PHP/Laravel 03課題

--- practice.php
+++ practice.php
@@ -41,3 +41,44 @@ $calendar_2018 = [
 // 12月を表示する
 echo $calendar_2018["December"];
 echo "\n";
+
+/*PHP/Laravel 03*/
+//課題1
+$name = "kuroda";
+if($name == "kuroda"){
+  echo "私は　あなたの　名前　です";
+}else{
+  "あなたの名前ではありません";
+}
+echo "\n";
+
+//課題2
+$total = 0;
+echo $total;
+//=> 0 と表示される。
+for ($i = 0; $i < 10000; $i++) {
+  $total += $i;
+}
+echo $total;
+echo "\n";
+
+//課題3
+$fruits = array("みかん","メロン","ぶどう","桃","梨");
+foreach($fruits as $fruits){
+  echo $fruits;
+  echo "\n";
+  
+//課題4
+/* for文の始めの値を定義する /
+$start = 1;
+/* for文の終わりの値を定義する */
+$end = 100;
+
+for($i = $start; $i < $end; $i++){
+// 5で割り切れたら{}内を実行する
+  if($i /= 5){
+echo $i;
+}
+}
+echo "\n";
+
